875e08cd49545773bae219f8dfd87cebed74b90d
Stuart Bate <bate.stuart93@gmail.com>
2023-03-15 11:24:41 -0700

    arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name){
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat(name){
+    return cats.shift()
+}
+
+function appendCat(name){
+   let newCats = [...cats]
+   newCats.push(name)
+   return newCats
+}
+
+
+function prependCat(name){
+    let newCats = [name,...cats]
+    return newCats
+}
+
+
+function removeLastCat(name){
+    let newCats = [...cats]
+    newCats.pop()
+    return newCats
+}
+
+function removeFirstCat(name){
+    let newCats = [...cats]
+    newCats.shift()
+    return newCats
+
+}
